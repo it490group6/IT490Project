96fddd652d56a01d24dc302cc57583aa2605616c
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-10-05 17:31:12 -0400

    Update testRabbitMQServer.php

--- RabbitMQ/testRabbitMQServer.php
+++ RabbitMQ/testRabbitMQServer.php
@@ -4,14 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function doLogin($username,$password)
-{
-    // lookup username in databas
-    // check password
-    return true;
-    //return false if not valid
-}
-
+//  This test server only prints out whatever array it gets
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
@@ -20,14 +13,8 @@ function requestProcessor($request)
   {
     return "ERROR: unsupported message type";
   }
-  switch ($request['type'])
-  {
-    case "login":
-      return doLogin($request['username'],$request['password']);
-    case "validate_session":
-      return doValidate($request['sessionId']);
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+
+  return array("returnCode" => '0', 'message'=>"Server received request and pro>
 }
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
