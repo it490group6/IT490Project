abe8162db739212be8099e2c12949f5e6ab3e966
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-10-16 19:14:24 -0400

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -31,3 +31,41 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         <li class="tab"><a href="#signup">Create Account</a></li>
         <li class="tab active"><a href="#login">Log In</a></li>
       </ul>
+      <div class="tab-content">
+
+         <div id="login">
+          <h1>Log in to continue</h1>
+
+          <form action="index.php" method="post" autocomplete="off">
+
+            <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="username" required autocomplete="off" name="username"/>
+          </div>
+
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password" required autocomplete="off" name="password"/>
+          </div>
+
+
+
+          <button class="button button-block" name="login" />Log In</button>
+
+          </form>
+
+        </div>
+
+
+      </div><!-- tab-content -->
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
