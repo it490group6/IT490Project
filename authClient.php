a9e421a679883c7a626617e8524afacc6552dd88
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-10-16 19:10:45 -0400

    Update authClient.php

--- authClient.php
+++ authClient.php
@@ -1,29 +1,75 @@
-equire_once('get_host_info.inc');
+
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function authentication($email, $pass){
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-if (isset($argv[1]))
+function authentication($email,$userpass)
 {
-  $msg = $argv[1];
+
+      $host = 'localhost';
+      $user = 'usr';
+      $dbpass = 'password';
+      $db = 'accounts';
+      $mysqli = new mysqli($host,$user,$dbpass,$db);
+      if($mysqli->connect_errno){
+        echo "\nMaster server down, switching to backup...\n";
+        $host = '192.168.43.25';
+        $user = 'usr';
+        $dbpass = 'password';
+        $db = 'accounts';
+ $mysqli = new mysqli($host,$user,$dbpass,$db) or die($mysqli->error);
+      }
+      else{
+        echo "Using master.\n";
+      }
+
+      $userinfo = array();
+
+      $email = $mysqli->escape_string($email);
+      $result = $mysqli->query("SELECT * FROM users WHERE email='$email' and pa>
+
+      $user = $result->fetch_assoc();
+
+      if ( $result->num_rows == 0 ){ // User doesn't exist
+         echo "Incorrect Credentials\n";
+          return false;
+      }
+      else { // User exists
+          echo "Correct Credentials, logging in...\n";
+          $userinfo['email'] = $user['email'];
+          $userinfo['first_name'] = $user['first_name'];
+          $userinfo['last_name'] = $user['last_name'];
+          $userinfo['active'] = $user['active'];
+          return json_encode($userinfo);
+      }
+
 }
-else
+function requestProcessor($request)
 {
-  $msg = "login";
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "Login":
+      return authentication($request['email'],$request['password']);
+      break;
+    case "Register":
+      return registration($request['firstname'],$request['lastname'],$request['>
+      break;
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and pro>
 }
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
-$request = array();
-$request['type'] = "Login";
-$request['email'] = $email;
-$request['password'] = $pass;
-$request['message'] = $msg;
-$response = $client->send_request($request);
-/$response = $client->publish($request);
-
-echo "client received response: ".PHP_EOL;
-return ($response);
-echo "\n\n";
-
-echo $argv[0]." END".PHP_EOL;
-}
+$server->process_requests('requestProcessor');
+exit();
 ?>
+
+
+
