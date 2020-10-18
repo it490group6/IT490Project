c508f9e6b97c6d85a703ceeda40c91b1f85bab97
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-10-18 00:46:18 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
-// Database Connection Setting Remotely 
-$hostname= "25.78.212.215" ;
+$hostname="10.192.239.223";
 $username="testuser";
 $password="Dipish_123!";
 $database="IT490PG6";
-$mysqli = new mysqli($hostname, $username, $password, $database);
-$mysqli->select_db($database) or die( "Unable to select database");
-print "<br>Successfully connected to MySQL.<br>";
 ?>
