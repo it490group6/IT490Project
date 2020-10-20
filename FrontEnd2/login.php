b33e87c03aba965a082919b2ad5f2aef0593afcb
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-10-19 20:51:00 -0400

    Update login.php

--- FrontEnd2/login.php
+++ FrontEnd2/login.php
@@ -5,55 +5,53 @@ require_once('get_host_info.inc');
 require_once('path.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-$date = date("Y-m-d", time());
 
 
-$username = htmlspecialchars($_POST['username']);
-$password =  htmlspecialchars($_POST['password']);
+$username = ($_POST['username']);
+$password =  ($_POST['password']);
 
-//$username = 'testuser2';//htmlspecialchars($_POST['username']);
-//$password =  '1234';//htmlspecialchars($_POST['password']);
 
 $error = '';
 
 if (isset($_POST['register'])) {
-	require "register.html";
+        require "register.html";
 
 } elseif (true) {
 
-	if (empty($username) || empty($password)) {
-		$error = "Oops! Invalid Username/Password";
-		header('Location: index.html');
-		die();
-		
-	} else {
-		$request = array();
-		$request['type'] = "login";
-		$request['username'] = $username;
-		$request['password'] = $password;
-		$request['message'] = "'{$userame}' requests to login '{$date}'";
-		
-		$response = $client->send_request($request);
+        if (empty($username) || empty($password)) {
+                $error = "Oops! Invalid Username/Password";
+                header('Location: index.html');
+                die();
 
-		if ($response === '401') {
-			$error = "Oops! Invalid Username/Password";
-			header('Location: index.html');
-			die();
-			
-		} elseif ($response === '404') {
-			$error = "Oops! Username not found!";
-			header('Location: index.html');
-			die();
-		
-		} else {
-			$_SESSION['username'] = $response[0]['username'];
-			$_SESSION['firstname'] = $response[0]['firstname'];
-			$_SESSION['lastname'] = $response[0]['lastname'];
-			header("Location: success.php");
-		}
-	}
+        } else {
+                $request = array();
+                $request['type'] = "login";
+                $request['username'] = $username;
+                $request['password'] = $password;
+                $request['message'] = "'{$username}' requests to login";
+
+                $response = $client->send_request($request);
+ 		
+		if ($response === '1') {
+                        $error = "Oops! Invalid Username/Password";
+                        header('Location: index.html');
+                        die();
+
+                } elseif ($response === '2') {
+                        $error = "Oops! Username not found!";
+                        header('Location: index.html');
+                        die();
+
+                } else {
+                        $_SESSION['username'] = $response[0]['username'];
+                        $_SESSION['firstname'] = $response[0]['firstname'];
+                        $_SESSION['lastname'] = $response[0]['lastname'];
+                        header("Location: success.php");
+                }
+        }
 
 }
 
 
 ?>
+
