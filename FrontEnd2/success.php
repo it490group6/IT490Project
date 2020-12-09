0cd13135905aba6ac03ad9113be7cb18981876d4
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-12-08 19:23:20 -0500

    Update success.php

--- FrontEnd2/success.php
+++ FrontEnd2/success.php
@@ -1,14 +1,34 @@
+?php
+$username = ($_POST['username']);
+?>
 <!DOCTYPE html>
 <html>
 <body>
 
-<h1>Access Granted</h1>
+<h1>Welcome Back</h1>
+<input type= "button" onclick="window.location.href='http://www.pogfinder.com/about.php';"value="About" />
+<input type= "button" onclick="window.location.href='http://www.pogfinder.com/search.html';"value="Search" />
+<input type= "button" onclick="window.location.href='http://www.pogfinder.com/contact.php';"value="Contact" />
+<h2>Looking a Streamer?<?php echo $username?> </h2>
 
+</body>
 
+<body>
+ <picture>
+  <source media="(min-width:650px)" srcset="twitch.png">
+  <source media="(min-width:465px)" srcset="twitch.png">
+  <img src="twitch.png" alt="Twitch" style="width:auto;">
+</picture> 
+ 
 <p id="demo"></p>
 
 </body>
 </html>
+
+
+
+
+
 
 
 
