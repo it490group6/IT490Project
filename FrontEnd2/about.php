c4f21bb52775f76d0c27f6ea3f397981647fe62f
it490group6 <71785257+it490group6@users.noreply.github.com>
2020-12-13 19:20:47 -0500

    Create about.php

---
+++ FrontEnd2/about.php
@@ -0,0 +1,90 @@
+<html lang="en">
+<head>
+<title>About</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+/* Style the header */
+.header {
+  background-color: #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}
+
+/* Style the top navigation bar */
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+/* Style the topnav links */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change color on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 15px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media screen and (max-width:600px) {
+  .column {
+    width: 100%;
+  }
+}
+</style>
+</head>
+<body>
+<div class="header">
+<h2>About</h2>
+<div class="topnav">
+  <div class="topnav-centered">
+  <a href="success.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="search.php">Stream Search</a>
+    <a href="contact.php">Contact</a>
+    <a href="logout.php">Logout</a>
+  </div>
+</div>
+<p><br><br></p>
+<p>Welcome to Pogfinder.com! We welcome you to this new website that will aide you in finding the right streamer for you. Have you ever been on twitch.tv, looking for a streamer, but you didn't know who to watch? Well this site is perfect for you. You see, it is easy to find big streamers who are at the top of the section of game they are playing, but why can you never find new people. Here you can set filters to find someone for your liking. For example, filtering out how many people are watching them right now, what games they are playing, are they live,etc.</p>
+
+</body>
+
+<body>
+
+ 
+<p id="demo"></p>
+
+</body>
+</html>
